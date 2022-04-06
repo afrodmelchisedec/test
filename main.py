479bb24955a90c44d12a13408ce0b0103b774ff5
Mwine Afrod <mwineafrod@gmail.com>
2022-04-06 09:52:40 +0300

    Altered main.py for CI 2

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # This is a sample Python script.
-
 # Press Shift+F10 to execute it or replace it with your code.
-
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
@@ -13,6 +11,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 # VERSION 1 - AFROD
